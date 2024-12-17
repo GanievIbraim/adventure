dd84be59cc6c97fd4dcd782ad520bc2e9650a69f
Eldar Temirov <temirov.e.i.21@gmail.com>
2024-12-17 18:45:56 +0300

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Включение CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Разрешённый источник (фронтенд)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Разрешённые методы
+    allowedHeaders: 'Content-Type, Accept', // Разрешённые заголовки
+    credentials: true, // Разрешить отправку учётных данных (если необходимо)
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
